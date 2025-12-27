bde52b232b114fc7a0a782409c9fbde54d6b37fe
JJRTechSec <jjrtechsec@gmail.com>
2025-12-27 16:09:12 +0100

    Each button shows meals in list when clicked. Problems to resolve: - Multiple lists when clicked. - Does not remove list when clicked a second time

--- Dinner Ideas/dinners.js
+++ Dinner Ideas/dinners.js
@@ -10,11 +10,13 @@ let weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturda
 
 const list = document.createElement('ul');
 const dinner = document.createElement('li');
-let chickenList = document.querySelector('.list');
 
-let button = document.querySelector('button');
+// CHICKEN
+let chickenList = document.querySelector('.chicken');
 
-button.addEventListener('click', function () {
+let chickenButton = document.getElementById('chickenButton');
+
+chickenButton.addEventListener('click', function () {
   console.log('Chicken button clicked');
 
   for (meal in chickenMeals) {
@@ -24,4 +26,77 @@ button.addEventListener('click', function () {
     list.append(dinner);
     chickenList.append(list);
   }
-})
+
+  chickenButton.addEventListener('click', function () {
+    console.log('Closed chicken list');
+    chickenList.removeChild(list);
+  })
+});
+
+
+// MINCE
+let minceList = document.querySelector('.mince');
+let minceButton = document.getElementById('minceButton');
+
+minceButton.addEventListener('click', function () {
+  console.log('Mince button clicked');
+
+  for (meal in minceMeals) {
+    const list = document.createElement('ul');
+    const dinner = document.createElement('li');
+    dinner.textContent = minceMeals[meal];
+    list.append(dinner);
+    minceList.append(list);
+  }
+});
+
+
+// PLANT-BASED
+let plantList = document.querySelector('.plant');
+let plantButton = document.getElementById('plantButton');
+
+plantButton.addEventListener('click', function () {
+  console.log('Plant button clicked');
+
+  for (meal in plantBased) {
+    const list = document.createElement('ul');
+    const dinner = document.createElement('li');
+    dinner.textContent = plantBased[meal];
+    list.append(dinner);
+    plantList.append(list);
+  }
+});
+
+
+// OTHER
+let otherList = document.querySelector('.other');
+let otherButton = document.getElementById('otherButton');
+
+otherButton.addEventListener('click', function () {
+  console.log('Other button clicked');
+
+  for (meal in other) {
+    const list = document.createElement('ul');
+    const dinner = document.createElement('li');
+    dinner.textContent = other[meal];
+    list.append(dinner);
+    otherList.append(list);
+  }
+});
+
+
+// CARB
+let carbList = document.querySelector('.highCarb');
+let carbButton = document.getElementById('carbButton');
+
+carbButton.addEventListener('click', function () {
+  console.log('Carb button clicked');
+
+  for (meal in highCarb) {
+    const list = document.createElement('ul');
+    const dinner = document.createElement('li');
+    dinner.textContent = highCarb[meal];
+    list.append(dinner);
+    carbList.append(list);
+  }
+});
