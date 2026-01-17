14017ecedda5954a0995c1214444e6310f16a546
JJRTechSec <jjrtechsec@gmail.com>
2026-01-17 02:02:46 +0100

    - Added cursor:pointer to task button - Added list to HTML - Got list to append, but only checkbox is appearing, or no checkbox and task/delete button show

--- myProjects/todo-list/todo.js
+++ myProjects/todo-list/todo.js
@@ -1,19 +1,24 @@
-const task = document.getElementById('task-input').value;
+const task = document.querySelector('#task-input');
 const taskButton = document.querySelector('button');
+const list = document.querySelector('#list');
 
 const listContainer = document.createElement('div');
 const checkbox = document.createElement('input', type = 'checkbox');
-const newTask = document.createElement('p');
+const newTask = document.createElement('li');
 const deleteTask = document.createElement('button');
 
 taskButton.addEventListener('click', function () {
-  if (task.length() > 0) {
-    const listContainer = document.createElement('div');
-    const checkbox = document.createElement('input', type = 'checkbox');
+  if (task.value.trim() != '') {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
     const newTask = document.createElement('p');
     const deleteTask = document.createElement('button');
 
-    newTask.textContent = task;
+    newTask.textContent = task.value;
+    deleteTask.textContent = '❌';
+    newTask.append(deleteTask);
+    checkbox.append(newTask);
+    list.append(checkbox);
 
     task.value = '';
     task.focus();
