b46f332ce8ed08149b15d242a9f324f390a84d11
JJRTechSec <jjrtechsec@gmail.com>
2026-01-18 00:48:48 +0100

    - All 3 items of task create and delete as needed - Just an issue with the width

--- myProjects/todo-list/todo.js
+++ myProjects/todo-list/todo.js
@@ -2,25 +2,32 @@ const task = document.querySelector('#task-input');
 const taskButton = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const listContainer = document.createElement('div');
-const checkbox = document.createElement('input', type = 'checkbox');
-const newTask = document.createElement('li');
-const deleteTask = document.createElement('button');
+
+
 
 taskButton.addEventListener('click', function () {
   if (task.value.trim() != '') {
+    const listContainer = document.createElement('div');
+    listContainer.classList.add('list-container');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    const newTask = document.createElement('p');
+    const newTask = document.createElement('li');
     const deleteTask = document.createElement('button');
 
     newTask.textContent = task.value;
     deleteTask.textContent = '❌';
+    
     newTask.append(deleteTask);
-    checkbox.append(newTask);
-    list.append(checkbox);
+    listContainer.append(checkbox, newTask, deleteTask);
+    list.append(listContainer);
 
     task.value = '';
     task.focus();
+
+    deleteTask.addEventListener('click', function () {
+      listContainer.replaceChildren();
+      listContainer.remove();
+      task.focus();
+    })
   }
 })
