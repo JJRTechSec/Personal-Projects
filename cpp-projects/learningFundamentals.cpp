7deb1f37c3dbeeda6cd5318c7cee0ed4a28ad2ca
JJRTechSec <jjrtechsec@gmail.com>
2026-01-22 01:15:43 +0100

    - Created hellowWorld in C++ - Started fundamentals learning in C++

---
+++ cpp-projects/learningFundamentals.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum = 15;
+  cout << myNum; //cout is an OBJECT
+  int result = myNum * 3;
+  cout << "\n" << result;
+  cout << "Can also use 'endl' to create a new line" << endl; // LOWERCASE L, not 1
+  return 0; // required to end the main function
+}
