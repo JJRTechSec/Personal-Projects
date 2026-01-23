4654c62c5af073045d1182371b086ed8406e9737
JJRTechSec <jjrtechsec@gmail.com>
2026-01-23 14:28:54 +0100

    - Added Roboto to every HTML doc - Finally fixed the Desktop bug of enlarging the header when clicked (JS issue) - Updated general.js to fix header issue - Increased margin on main element

--- scripts/general.js
+++ scripts/general.js
@@ -3,6 +3,8 @@ const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {
-  mainnav.classList.toggle('show');
-  hambutton.classList.toggle('show');
+  if (window.innerWidth < 1200) {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+  }
 })
