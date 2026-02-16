6b4027f793fe9bf28c7cac9b2cfcfe608a26caf5
JJRTechSec <jjrtechsec@gmail.com>
2026-02-17 00:22:30 +0100

    - Fixed the code and removed excess, so now it adds and displays the names underneath

--- scripts/javascriptSandbox.js
+++ scripts/javascriptSandbox.js
@@ -3,27 +3,25 @@ const main = document.getElementById('main');
 let input = document.createElement('input');
 main.appendChild(input);
 
-//create name
-let username = input.value;
-
 //create button to register the name
 let addButton = document.createElement('button');
 addButton.textContent = 'Add Name';
 // append button to main
 main.appendChild(addButton);
 
-// add element to show name underneath
-let addedName = document.createElement('h4');
-addedName.textContent = username;
-let nameContainer = document.createElement('div');
-
 //clear input and restore focus to input when clicked
 // add name underneath when clicked
 addButton.addEventListener('click', function () {
+  //Create container and element type for name display
   let nameContainer = document.createElement('div');
   let addedName = document.createElement('h4');
+  // Assign input to h4 element
+  let username = input.value;
+  addedName.textContent = username;
+  // append all to main
   nameContainer.appendChild(addedName);
   main.appendChild(nameContainer);
+  // clear input and reset focus
   input.value = '';
   input.focus();
 })
