9eaf2b2a29a6d8d50b3f0705c8d28390c5583fac
JJRTechSec <jjrtechsec@gmail.com>
2026-02-17 00:45:19 +0100

    - Added delete button and edited the css to align button next to name, and it deletes the desired name on click

--- scripts/javascriptSandbox.js
+++ scripts/javascriptSandbox.js
@@ -9,19 +9,33 @@ addButton.textContent = 'Add Name';
 // append button to main
 main.appendChild(addButton);
 
+//create div to contains names
+let nameContainer = document.createElement('div');
+nameContainer.classList.add('name-container');
+
 //clear input and restore focus to input when clicked
 // add name underneath when clicked
 addButton.addEventListener('click', function () {
-  //Create container and element type for name display
-  let nameContainer = document.createElement('div');
+  // create element type for name display
   let addedName = document.createElement('h4');
   // Assign input to h4 element
   let username = input.value;
   addedName.textContent = username;
+  //Create delete button
+  let deleteButton = document.createElement('button');
+  deleteButton.innerHTML = '&#10060';
+  deleteButton.classList.add('delete-button')
   // append all to main
   nameContainer.appendChild(addedName);
+  nameContainer.appendChild(deleteButton);
   main.appendChild(nameContainer);
   // clear input and reset focus
   input.value = '';
   input.focus();
+
+  // Event to delete name and button on selected name
+  deleteButton.addEventListener('click', function () {
+    nameContainer.removeChild(addedName);
+    nameContainer.removeChild(deleteButton);
+  })
 })
